41a32e5eef8b147c4bcd62fdb5ea59e934892d24
Tucker Adkison <tuckeroa@gmail.com>
2026-06-18 20:49:42 -0600

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > j) {
+                return false;
+            }
+
+            j = Math.max(j, nums[i] + i);
+        }
+
+        return true;
+    }
+}
