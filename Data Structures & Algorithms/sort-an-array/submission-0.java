574ae69b122d250996e15dd798f4f945b9c9954f
Tucker Adkison <tuckeroa@gmail.com>
2026-08-27 16:58:46 -0500

    Add: sort-an-array - submission-0

---
+++ Data Structures & Algorithms/sort-an-array/submission-0.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        return mergeSort(0, nums.length-1, nums);
+    }
+
+    public int[] mergeSort(int start, int end, int[] nums) {
+        if (start < end) {
+            int mid = (int) Math.floor((start+end) / 2);
+            
+            int[] left = mergeSort(start, mid, nums);
+            int[] right = mergeSort(mid+1, end, nums);
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            int mergedLength = left.length + right.length;
+            int[] merged = new int[mergedLength];
+
+            while (k < merged.length) {
+                if (i == left.length) {
+                    merged[k] = right[j];
+                    j++;
+                } else if (j == right.length) {
+                    merged[k] = left[i];
+                    i++;
+                } else if (left[i] <= right[j]) {
+                    merged[k] = left[i];
+
+                    i++;
+                } else {
+                    merged[k] = right[j];
+
+                    j++;
+                }
+                k++;
+            }
+
+            System.out.println(Arrays.toString(left));
+            System.out.println(Arrays.toString(right));
+            System.out.println(Arrays.toString(merged));
+
+            return merged;
+        } else {
+            return new int[] {nums[start]};
+        }
+    }
+}
